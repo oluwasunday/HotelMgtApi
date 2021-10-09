fe79cecd6249d3d8784c45e1f8b79ac7056f4de4
oluwasunday <sundayoladejo13@gmail.com>
2021-10-10 00:19:29 +0100

    confirm email added

---
+++ HotelMgt.Utilities/Validations/AuthenticationValidators/ConfirmEmailDtoValidator.cs
@@ -0,0 +1,24 @@
+﻿using FluentValidation;
+using HotelMgt.Dtos.AuthenticationDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelMgt.Utilities.Validations.AuthenticationValidators
+{
+    public class ConfirmEmailDtoValidator : AbstractValidator<ConfirmEmailDto>
+    {
+        public ConfirmEmailDtoValidator()
+        {
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .WithMessage("Email is required");
+            RuleFor(x => x.Token)
+                .NotEmpty()
+                .NotNull()
+                .WithMessage("No token generated");
+        }
+    }
+}
